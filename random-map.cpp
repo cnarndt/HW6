773276f444c3adb204d16042a164888bf117e14b
cnarndt <70283233+cnarndt@users.noreply.github.com>
2020-11-10 10:32:44 -0900

    Initial commit

---
+++ random-map.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::map;
+
+int main() {
+    std::random_device r;
+
+    std::default_random_engine e1(r());
+    std::uniform_real_distribution<int> uniform_dist(1, 6);
+    int mean = uniform_dist(e1);
+    cout<<"Randomly-chosen mean:"<<mean<< '\n';
+
+    std::seed_seq seed2{r(), r(), r(), r(), r(),r(), r(),r()};
+    std::mt19937  e2(seed2);
+    std::normal_distribution<> normaldist(mean, 2);
+
+    map<int, int> hist;
+    for(int n = 0; n < 10000; ++n){
+        ++hist[std::round(normal_dist(e2))];
+    }
+    cout<<"Normal distribution around" << mean << "\n";
+    for (auto p : hist){
+        cout<< std::fixed<< std::setprecision(1)<< std::setw(2)
+        <<p.first<< ' '<< std::string(p.second/200, '*')<< '\n';
+    }
+}
