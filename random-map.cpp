b4e9b78023482f4d26a076a446bdd4780c542f88
cnarndt <70283233+cnarndt@users.noreply.github.com>
2020-11-11 09:12:31 -0900

    started on main

--- random-map.cpp
+++ random-map.cpp
@@ -11,3 +11,20 @@
 #include <vector>
 #include "RandomBetween.h"
 
+int main(){
+    // Seed with a real random value, if available
+    std::random_device r;
+
+// For random Range
+    std::default_random_engine e1(r());
+    std::default_random_engine e3(r()+1);
+
+    std::uniform_int_distribution<int> uniform_dist(1, 5);
+    std::uniform_int_distribution<int> uniform_dist2(6, 10);
+//Setting range
+    int first = 2;
+    int last =25;
+    std::seed_seq seed2{r(), r(), r(), r(), r(), r(), r(), r()};
+    std::mt19937 e2(seed2);
+
+}
