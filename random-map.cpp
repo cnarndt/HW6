41418e1ebb89f534a7c8a47d10acb101e7d4b4f7
cnarndt <70283233+cnarndt@users.noreply.github.com>
2020-11-11 08:32:40 -0900

    RandombetweenU

--- random-map.cpp
+++ random-map.cpp
@@ -1,9 +1,14 @@
+//Clayton Arndt
+//11/11/2020
+//CS201
+
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <map>
 #include <random>
 #include <cmath>
+#include "RandomBetween.h"
 using std::cout;
 using std::cin;
 using std::endl;
